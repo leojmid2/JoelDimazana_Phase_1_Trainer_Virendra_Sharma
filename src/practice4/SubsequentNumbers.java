bf8795c76fb8d1647bb82d0f1a997b6a325e08c1
leojmid2 <leojmid@gmail.com>
2021-03-03 15:01:55 -0800

    Phase 1 practice 4 - added a function to show the numbers and total count of subsequent number

--- src/practice4/SubsequentNumbers.java
+++ src/practice4/SubsequentNumbers.java
@@ -1,5 +1,6 @@
 package practice4;
 
+import java.util.ArrayList;
 
 /*
  * Find longest increasing subsequent numbers where n < 100
@@ -9,6 +10,8 @@ package practice4;
 public class SubsequentNumbers {
 	
 	static int[] num = {1,2,4,5,6,9,100,10,11,12,13,14,1}; 
+	static ArrayList<Integer> temp =  new ArrayList<Integer>();
+	static ArrayList<Integer> longest =  new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 		int count = 1;
@@ -16,27 +19,32 @@ public class SubsequentNumbers {
 		int next =1;
 		
 			for (int i=0;i<num.length;i++) {
-				System.out.println("i " +i);
 				if (num[i] >= 100 ) {
-					System.out.println(">100");
 					next++;
 					continue;
 				}	
 
 				if(next<num.length && (num[i]+1)==num[next]) {
 					count+=1;
+					temp.add(num[i]);
 				}
 				else { 
 
 					if (count > highest) {
 						 highest = count;
+						 temp.add(num[i]); //to get the last data
+						 longest.clear();
+						 longest.addAll(temp);
+						 temp.clear();
+						 
 					}
 					count=1;
 				}	
 				next++;
 				
 			}
-		System.out.println("The longes increasing subsequent number is " +highest);
+		System.out.println("Total count of increasing subsequent number: " +highest);
+		System.out.println("Longest increasing subsequent number:" + longest.toString());
 		}
 	}
 	
